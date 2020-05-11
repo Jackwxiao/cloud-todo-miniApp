b7a20093e25c3a779345d455cf063790b2e88664
wuxiao <1291383707@qq.com>
2020-05-11 17:06:04 +0800

    实现图片添加以及上传

--- miniprogram/pages/addTodo/addTodo.js
+++ miniprogram/pages/addTodo/addTodo.js
@@ -2,16 +2,43 @@
 const db = wx.cloud.database()
 const todos = db.collection('todos')
 Page({
-onSubmit:  function(event){
-  console.log(event.detail.value.title)
+  data:{
+    image: null
+  },
+  selectImage: function(e){
+    wx.chooseImage({
+      success: res => {
+        console.log(res.tempFilePaths[0])
+        wx.cloud.uploadFile({
+          cloudPath: `${Math.floor(Math.random()*10000000)}.png`,
+          filePath: res.tempFilePaths[0]
+        }).then(res => {
+          console.log(res.fileID)
+          this.setData({
+            image: res.fileID
+          })
+        }).catch(err => {
+          console.error(err)
+        })
+      },
+    })
+  },
+  onSubmit:  function(event){
   todos.add({
     data: {
-      title: event.detail.value.title
+      title: event.detail.value.title,
+      image: this.data.image
     }
   }).then(res =>{
+    console.log(res._id)
     wx.showToast({
-      title: 'Success',
-      icon: 'success'
+      title: '添加成功',
+      icon: 'success',
+      success:res2 => {
+        wx.redirectTo({
+          url: `../todoInfo/todoInfo?id=${res._id}`,
+        })
+      }
     })
   })
 }
